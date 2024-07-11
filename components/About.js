d103871339ffa332f58d5a2d942834edb585db17
Steven Vu <vuhoangsteven@gmail.com>
2024-07-10 20:10:30 -0700

    pushing about and home components

---
+++ components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/About.css';
+
+const About = () => (
+    <section id="about">
+        <h2>About Me</h2>
+        <p></p>
+    </section>
+)
---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/Home.css';
+const Home = () => (
+    <section id="home">
+        <h1>Welcome to My Portfolio</h1>
+        <p>I'm Steven Vu, a Software Developer</p>
+        <a href="#about" className="btn">Learn More</a>
+    </section>
+);
+
+export default Home;
